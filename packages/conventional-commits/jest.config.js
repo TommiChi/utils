6726476cb64b7c1ccb05d51ab6ce93a029e73b59
Tom Nchinda <tom@passionatepeople.io>
2025-06-04 14:11:38 +0200

    ♻️chore(all | monorepo): add Github CI process

--- packages/conventional-commits/jest.config.js
+++ packages/conventional-commits/jest.config.js
@@ -1,7 +1,7 @@
-/** @type {import("jest").Config} **/
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // roots: ['<rootDir>/src'],
   collectCoverage: true,
   coverageDirectory: '<rootDir>/coverage',
   coverageReporters: ['text', 'lcov'],
